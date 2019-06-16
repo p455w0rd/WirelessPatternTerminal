67382a4ffd302adc412a8a5242851b2049c16c0c
p455w0rd <p455w0rd@gmail.com>
2019-06-16 18:21:01 -0400

    version bump for maven

--- src/main/java/p455w0rd/wpt/init/ModGlobals.java
+++ src/main/java/p455w0rd/wpt/init/ModGlobals.java
@@ -21,7 +21,7 @@ import p455w0rd.ae2wtlib.init.LibGlobals;
 public class ModGlobals {
 
 	public static final String MODID = "wpt";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String NAME = "Wireless Pattern Terminal";
 	public static final String SERVER_PROXY = "p455w0rd.wpt.proxy.CommonProxy";
 	public static final String CLIENT_PROXY = "p455w0rd.wpt.proxy.ClientProxy";
